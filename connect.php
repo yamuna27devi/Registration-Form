c3121140d5855c48ae6941a5db02fe84f8a8115b
yamuna27devi <89919198+yamuna27devi@users.noreply.github.com>
2021-10-29 08:12:45 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$number = $_POST['number'];
+
+
+$conn = new mysqli('localhost','root','','DATA');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number)values(?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi",$firstName, $lastName, $gender, $email, $password, $number);
+    $stmt->execute();
+    echo"registration successfully...";
+    $stmt->close();
+    $conn->close();
+}
+?>
